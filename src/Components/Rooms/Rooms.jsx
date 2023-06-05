e34f6f55676e8a07c91bf72e2f09d7fa630352a3
Sazzads <sazzadtk@gmail.com>
2023-06-05 23:32:48 +0600

    add code

---
+++ src/Components/Rooms/Rooms.jsx
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from 'react';
+import Container from '../Shared/Container';
+import Card from './Card';
+import Loader from '../Shared/Loader/Loader';
+import { useSearchParams } from 'react-router-dom';
+
+const Rooms = () => {
+    const [rooms, setRooms] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [params, setParams] = useSearchParams();
+    const category = params.get("category")
+    console.log(category);
+
+
+    useEffect(() => {
+        setLoading(true)
+        fetch('rooms.json')
+            .then(res => res.json())
+            .then(data => {
+                if (category) {
+                    const filtered = data.filter(room => room.category === category)
+                    setRooms(filtered)
+                }
+                setRooms(data)
+                setLoading(false)
+            })
+
+            .catch(err => console.log(err))
+    }, [category])
+    if (loading) {
+        return <Loader></Loader>
+    }
+    return (
+        <Container>
+            <div className="pt-12 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
+                {
+                    rooms.map((room, index) => <Card key={index} room={room}></Card>)
+                }
+            </div>
+        </Container>
+    );
+};
+
+export default Rooms;
